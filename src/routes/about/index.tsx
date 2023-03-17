9ed4394009fe51e95461a468991a15db1c7c0862
Mahmoud Guefri <Mamouty4@gmail.com>
2023-03-17 14:52:49 +0100

    Add routes to the app
    • Create folders inside the routes one, each with a name of its route.
    • Inside each route folder create an index.tsx file and put the content of the route in it.
    • Delete the flowers and todolist folders.

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    console.log('hello')
+    return (
+        <article>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aut et assumenda. Obcaecati perspiciatis error dicta, ducimus enim, natus rerum officia vero consectetur dolores voluptate aliquid quia laborum laudantium ipsum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aut et assumenda. Obcaecati perspiciatis error dicta, ducimus enim, natus rerum officia vero consectetur dolores voluptate aliquid quia laborum laudantium ipsum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aut et assumenda. Obcaecati perspiciatis error dicta, ducimus enim, natus rerum officia vero consectetur dolores voluptate aliquid quia laborum laudantium ipsum!</p>
+
+        </article>
+    )
+})
---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <article>
+            <h2>Contact</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aut et assumenda. Obcaecati perspiciatis error dicta, ducimus enim, natus rerum officia vero consectetur dolores voluptate aliquid quia laborum laudantium ipsum!</p>
+
+        </article>
+    )
+})
