60f6f307a36a9582ff6cc2636cf004530ae00837
Mahmoud Guefri <Mamouty4@gmail.com>
2023-03-19 18:55:52 +0100

    Use the state hooks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,33 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useStore } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 
 export default component$(() => {
+  // The useSignal hook is used for simple variables such as numbers and strings.
+  const name = useSignal('Mamouty');
+  // The useStore hook is used for objects and arrays.
+  const person = useStore({ name: 'peach', age: 30});
+
+  const blogs = useStore([
+    {id: 1, title: 'my first blog'},
+    {id: 2, title: 'my second blog'},
+    {id: 3, title: 'marmite rules!'}
+  ])
+
   return (
     <div>
       <h2>Hello World</h2>
+      <p>Hello, {name.value}</p>
+      <p>Hello, {person.name} - {person.age}</p>  
+
+      <button onClick$={() => name.value = 'luigi'}>click me</button>
+      <button onClick$={() => person.name = 'bowser'}>click me</button>
+
+      {blogs.map(blog =>(
+        <div key={blog.id}>{blog.title}</div>
+      ))}
+
+      <button onClick$={() => blogs.pop()}>remove a blog</button>
     </div>
   );
 });
